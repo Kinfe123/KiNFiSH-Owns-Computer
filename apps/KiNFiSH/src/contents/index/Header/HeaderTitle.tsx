2d31dc58820275644c1e99b1a53b3beb253bb9c5
Kinfe123 <kinfishtech@gmail.com>
2023-07-30 22:24:19 +0300

    Some UI improvements

--- apps/KiNFiSH/src/contents/index/Header/HeaderTitle.tsx
+++ apps/KiNFiSH/src/contents/index/Header/HeaderTitle.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import { m, useAnimationControls } from 'framer-motion';
 import Image from 'next/image';
-
+import styles from './Header.module.css'
 const animation = {
   hide: { x: -32, opacity: 0 },
   show: {
@@ -69,14 +69,15 @@ function HeaderTitle() {
           transition={{ delay: 0.2 }}
         >
           I&apos;m{' '}
-          <span className="modern text-center block bg-gradient-to-r from-red-400 to-blue-500 bg-clip-text text-transparent ">
+          <span className={styles.magicText}>
             
+          < br />
             <strong>
-              KinfeMichael  <br /> 
+              KinfeMichael Tariku <br /> 
             </strong>{' '}
            
         </span>
-        <span>Tariku,{' '}</span> 
+        <span className={styles.magicStar}> </span> 
           
         </m.span>
         <m.h1
@@ -94,7 +95,7 @@ function HeaderTitle() {
             className={clsx(
               'font-bold text-center lowercase text-slate-700',
               'dark:text-slate-300'
-            )}
+             , styles.magicText2)}
           >
             Software Dev -  Competitive Programmer
           </strong>{' '}
@@ -106,6 +107,37 @@ function HeaderTitle() {
         </m.h1>
       </span>
     </div>
+  //   <div className="font-heading max-w-3xl text-4xl font-bold text-slate-800 dark:text-slate-300 ">
+  //   <h1 className=" hover:opacity-95 flex flex-wrap flex-col md:flex-row items-start md:item gap-2 dark:text-slate-100/80 2xl:text-4xl md:text-3xl text-lg">
+  //     <span className=" relative inline-block ">
+  //       <span className={styles.magicStar}>
+  //         <svg viewBox="0 0 512 512">
+  //           <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
+  //         </svg>
+  //       </span>
+  //       <span className={styles.magicStar}>
+  //         <svg viewBox="0 0 512 512">
+  //           <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
+  //         </svg>
+  //       </span>
+  //       <span className={styles.magicStar}>
+  //         <svg viewBox="0 0 512 512">
+  //           <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
+  //         </svg>
+  //       </span>
+  //       <span className={styles.magicText}> Software Dev -  Competitive Programmer</span>
+  //     </span>{" "}
+  //     <span className="hidden md:block  bg-gradient-to-tr dark:from-slate-300 from-slate-500 to-slate-600 dark:to-slate-400 bg-clip-text text-transparent">
+      
+  //     </span>
+  //     <span className="  bg-gradient-to-tr hidden md:block dark:from-slate-300 from-slate-500 to-slate-600 dark:to-slate-400 bg-clip-text text-transparent">
+  //       <br />
+  //     </span>
+  //   </h1>
+  //   <h1 className="bg-gradient-to-br md:text-4xl xl:text-6xl text-5xl from-red-400 to-blue-600 bg-clip-text uppercase text-transparent dark:from-red-500 dark:to-blue-500">
+  //     Kinfe <br />Michael Tariku
+  //   </h1>
+  // </div>
   );
 }
 
